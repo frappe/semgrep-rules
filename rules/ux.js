a1ea14cb98436a19cb07e2ae1951dee41d59bec7
Ankush Menat <ankush@frappe.io>
2021-12-02 12:01:39 +0530

    feat: rule for missing button text translation (#2)

--- rules/ux.js
+++ rules/ux.js
@@ -7,3 +7,15 @@ frappe.msgprint('What');
 
 // ok: frappe-missing-translate-function-js
 frappe.throw('  {{ _("Both login and password required") }}.  ');
+
+
+// ruleid: frappe-missing-translation-button-text
+frm.add_custom_button("Transfer Asset", function() {
+
+})
+
+// ok: frappe-missing-translation-button-text
+frm.add_custom_button(__("Transfer Asset"), function() {
+
+})
+
