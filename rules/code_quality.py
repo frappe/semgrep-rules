ab333907083069c009c78dd3510d7486e64c648c
Ankush Menat <ankush@frappe.io>
2022-06-30 11:10:05 +0530

    rule: suggest use of FrappeTestCase and check super call

--- rules/code_quality.py
+++ rules/code_quality.py
@@ -11,3 +11,40 @@ def func(*args, **kwargs):
 # ok: overusing-args
 def func(*args):
   return 1
+
+
+
+import unittest
+from frappe.tests.utils import FrappeTestCase
+
+# ruleid: use-frappetestcase
+class MyTest(unittest.TestCase):
+	def test_reality(self):
+		self.assertEqual(1, 1)
+
+# ok: use-frappetestcase
+class MyTestGood(FrappeTestCase):
+	def test_reality(self):
+		self.assertNotEqual(1, 2)
+
+# ruleid: frappetestcase-missing-super-call
+class MyTestGood(FrappeTestCase):
+
+	def test_reality(self):
+		self.assertNotEqual(1, 2)
+
+	@classmethod
+	def setUpClass():
+		pass
+
+
+# ok: frappetestcase-missing-super-call
+class MyTestGood(FrappeTestCase):
+
+	def test_reality(self):
+		self.assertNotEqual(1, 2)
+
+	@classmethod
+	def setUpClass():
+		super().setUpClass()
+		print("starting test")
